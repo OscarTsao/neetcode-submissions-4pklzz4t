23e54f0c19a577b3d45c538e948258af00711e39
Yu-Ning Tsao <186167885+OscarTsao@users.noreply.github.com>
2026-06-05 19:52:41 +0800

    Add: n-th-tribonacci-number - submission-1

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int t0 = 0, t1 = 1, t2 = 1;
+        if (n == 0) return 0;
+        else if (n <= 2) return 1;
+        
+        int i = 3;
+        while (i <= n) {
+            int tmp = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = tmp;
+            i++;
+        }
+        return t2;
+    }
+};
