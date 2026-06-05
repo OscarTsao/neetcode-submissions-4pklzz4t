77c13ac8be6e31723ab793898bb70b73f0f20d1a
Yu-Ning Tsao <186167885+OscarTsao@users.noreply.github.com>
2026-06-05 16:24:15 +0800

    Add: valid-parenthesis-string - submission-2

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int leftMax = 0, leftMin = 0;
+
+        for (char c: s) {
+            if (c == '(') {
+                leftMax++;
+                leftMin++;
+            }
+            else if (c == ')') {
+                leftMax--;
+                leftMin--;
+            }
+            else {
+                leftMax++;
+                leftMin--;
+            }
+            if (leftMax < 0) return false;
+            if (leftMin < 0) leftMin = 0;
+        }
+        
+        return leftMin == 0;
+    }
+};
