52439fb0b2975c25c826b84a4926533f86b224a0
Yu-Ning Tsao <186167885+OscarTsao@users.noreply.github.com>
2026-06-05 16:02:48 +0800

    Add: gas-station - submission-5

---
+++ Data Structures & Algorithms/gas-station/submission-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+
+        for (int i = 0; i< n; i++) {
+            int cur = 0;
+            for (int j = 0; j < n & cur >= 0; j++) {
+                int index = (i+j) % n;
+                cur = cur + gas[index] - cost[index];
+            }
+            if (cur >= 0) return i;
+        }
+        return -1;
+    }
+};
