242f837aec6c521e499f9a84f31b81a0390156b5
Yu-Ning Tsao <186167885+OscarTsao@users.noreply.github.com>
2026-06-05 15:47:27 +0800

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> steps(n, 0);
+        if (n == 1) return 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= nums[i]; j++) {
+                if (i+j >= n) break;
+                if (steps[i+j]) steps[i+j] = min(steps[i+j], steps[i]+1);
+                else steps[i+j] = steps[i]+1;
+            }
+        }
+        return steps[n-1];
+    }
+};
