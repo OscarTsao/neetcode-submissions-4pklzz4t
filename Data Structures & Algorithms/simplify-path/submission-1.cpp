5c3401ad576f7f57004d29343066ee4997cfae6d
Yu-Ning Tsao <186167885+OscarTsao@users.noreply.github.com>
2026-06-11 20:16:05 +0800

    Add: simplify-path - submission-1

---
+++ Data Structures & Algorithms/simplify-path/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        stringstream ss(path);
+        string dir;
+
+        while (getline(ss, dir, '/')) {
+            if (dir == "" || dir == ".") {
+                continue;
+            }
+            else if (dir == "..") {
+                if (!st.empty()) {
+                    st.pop_back();
+                }
+            }
+            else {
+                st.push_back(dir);
+            }
+        }
+
+        if (st.empty()) {
+            return "/";
+        }
+
+        string res = "";
+        for (const string& s : st) {
+            res += ('/' + s);
+        }
+        return res;
+    }
+};
