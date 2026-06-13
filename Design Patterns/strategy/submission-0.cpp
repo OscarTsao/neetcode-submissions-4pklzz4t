7a7a5d6b963b98fc081da81c524915cc455574dc
Yu-Ning Tsao <186167885+OscarTsao@users.noreply.github.com>
2026-06-13 19:35:05 +0800

    Add: strategy - submission-0

---
+++ Design Patterns/strategy/submission-0.cpp
@@ -0,0 +1,70 @@
+class Person {
+private:
+    string lastName;
+    int age;
+    bool married;
+
+public:
+    Person(string lastName, int age, bool married) 
+        : lastName(lastName), age(age), married(married) {}
+
+    string getLastName() {
+        return lastName;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    bool isMarried() {
+        return married;
+    }
+};
+
+class PersonFilter {
+public:
+    virtual ~PersonFilter() {}
+    virtual bool apply(Person& person) = 0;
+};
+
+class AdultFilter : public PersonFilter {
+    // Implement Adult filter
+    bool apply(Person& person) {
+        return person.getAge() >= 18;
+    }
+};
+
+class SeniorFilter : public PersonFilter {
+    // Implement Senior filter
+    bool apply(Person& person) {
+        return person.getAge() >= 65;
+    }
+};
+
+class MarriedFilter : public PersonFilter {
+    // Implement Married filter
+    bool apply(Person& person) {
+        return person.isMarried();
+    }
+};
+
+class PeopleCounter {
+private:
+    PersonFilter* filter;
+
+public:
+    void setFilter(PersonFilter& filter) {
+        this->filter = &filter;
+    }
+
+    int count(vector<Person>& people) {
+        // Implement method here
+        int sum = 0;
+        for (Person& p : people) {
+            if (filter->apply(p)) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+};
