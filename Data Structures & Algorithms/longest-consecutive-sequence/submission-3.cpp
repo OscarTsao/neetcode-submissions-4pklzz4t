f8a706356d5c3672dbf826b8fcc9f5c46ef62615
Yu-Ning Tsao <186167885+OscarTsao@users.noreply.github.com>
2026-06-14 15:50:42 +0800

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> st;
+        int n = nums.size();
+        for (int x : nums) {
+            st.insert(x);
+        }
+        int ans = 0;
+
+        for (int x : nums) {
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (st.count(x-1)) {
+                    break;
+                }
+                if (st.count(x+i)) {
+                    count++;
+                }
+                else {
+                    break;
+                }
+            }
+            ans = max(ans, count);
+        }
+        return ans;
+    }
+};
